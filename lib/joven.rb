8e2fbdd82ae4bb150a62a68dc48fae8eed099771
andela-bmakinwa <makinwa37@gmail.com>
2016-03-10 12:17:27 +0100

    Create base joven application class

--- lib/joven.rb
+++ lib/joven.rb
@@ -1,5 +1,24 @@
 require "joven/version"
 
 module Joven
-  # Your code goes here...
+  class Application
+    def call(env)
+      @request = Rack::Request.new(env)
+      path = @request.path_info
+      request_method = @request.request_method.downcase
+      return [500, {}, []] if path == "/favicon.ico"
+      controller, action = get_controller_and_action(path, request_method)
+      response = controller.new.send(action)
+      [200, {"Content-Type" => "text/html"}, [response]]
+    end
+
+    def get_controller_action(path, verb)
+      _, controller_name, action_name = path.split("/")
+      require "#{controller_name.downcase}_controller.rb"
+      controller = Object.const_get("#{controller_name.capitalize!}Controller")
+      action = action_name.nil? ? verb : "#{verb}_#{action_name}"
+
+      [controller, action]
+    end
+  end
 end
