133c35e50a46979045e54561a0f547392cf18e57
andela-bmakinwa <makinwa37@gmail.com>
2016-03-11 19:17:14 +0100

    Add dependencies and write tests for the dependencies

---
+++ spec/unit/dependencies.rb
@@ -0,0 +1,12 @@
+require "spec_helper"
+$LOAD_PATH.unshift File.expand_path("../helpers", __FILE__)
+ 
+describe "Helpers Methods" do
+  context "#const_missing" do
+    it { expect("Person".to_constant).to eq Person }
+ 
+    it { expect("Todo".to_constant).to eq Todo }
+ 
+    it { expect("PersonController".to_constant).to eq PersonController }
+  end
+end
