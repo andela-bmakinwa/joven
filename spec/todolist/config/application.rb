b86f01b54afea1d54035c59135896ff08ee03ae3
andela-bmakinwa <makinwa37@gmail.com>
2016-03-15 16:27:05 +0100

    Include dependencies file to main Joven application

--- spec/todolist/config/application.rb
+++ spec/todolist/config/application.rb
@@ -4,9 +4,6 @@ $LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
 $LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "models")
 
 module Todolist
-  require "todolist_controller"
-  require "todo"
-
   class Application < Joven::Application
   end
 end
