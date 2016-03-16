b70d03706c28b83b588a6727b163f15686643188
andela-bmakinwa <makinwa37@gmail.com>
2016-03-16 10:26:56 +0100

    Change codeclimate and travis settings.

--- lib/joven/dependencies.rb
+++ lib/joven/dependencies.rb
@@ -1,6 +1,6 @@
 class Object
   def self.const_missing(const)
-    return if const.to_s == "rails"
+    return const if const.to_s == "rails"
     require const.to_s.to_snake_case
     Object.const_get(const)
   end
